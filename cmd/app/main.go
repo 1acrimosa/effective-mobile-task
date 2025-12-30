2489ac3ef21587dd1dde8daa29b5b5ee49eff16f
Dastan Amangeldi <dastanktl26@gmail.com>
2025-12-30 15:37:17 +0500

    upd config relocation

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"github.com/1acrimosa/effective-mobile-task/internal/config"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
-	"github.com/1acrimosa/effective-mobile-task/config"
 	"github.com/1acrimosa/effective-mobile-task/internal/repository"
 )
 
